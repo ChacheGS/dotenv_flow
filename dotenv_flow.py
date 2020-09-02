22a27cfbda078d810bc819b343e6c8cbb3d5f387
Carlos Gonzalez <gonsa.carlos@gmail.com>
2020-09-02 04:20:53 +0200

    removed args

--- dotenv_flow.py
+++ dotenv_flow.py
@@ -9,15 +9,13 @@ from dotenv import load_dotenv, find_dotenv
 logger = logging.getLogger(__name__)
 
 
-def dotenv_flow(raise_error_if_not_found: bool = False,
-                usecwd: bool = False,
-                stream: Union[StringIO, StringIO[str], None] = None,
-                verbose: bool = False,
+def dotenv_flow(usecwd: bool = False,
                 override: bool = False,
                 interpolate: bool = True,
                 **kwargs: Optional[str]) -> bool:
     """
-    Loads different dotenv files based on the value of the PY_ENV variable. Values in more specific files override previous values.
+    Loads different dotenv files based on the value of the PY_ENV variable.
+    Values in more specific files override previous values.
     Files have 2 versions:
         - public (ex: .env.dev) that should be committed to version control
         - private (ex: .env.dev.local) that has preference over the previous one if present
@@ -30,10 +28,6 @@ def dotenv_flow(raise_error_if_not_found: bool = False,
     # local .env* files
     .env.local
     .env.*.local
-    :param verbose:
-    :param stream:
-    :param usecwd:
-    :param raise_error_if_not_found:
     :param override:
     :param interpolate:
     :return:
@@ -47,10 +41,10 @@ def dotenv_flow(raise_error_if_not_found: bool = False,
     logger.debug(f"PY_ENV is {env}")
     for e in reversed(envs):
         for el in (f"{e}.local", e):
-            dotenv_path = find_dotenv(el, raise_error_if_not_found=raise_error_if_not_found, usecwd=usecwd)
+            dotenv_path = find_dotenv(el, usecwd=usecwd)
             if dotenv_path:
                 logger.info(f"loading {dotenv_path}")
-                load_dotenv(dotenv_path, stream=stream, verbose=verbose, override=override, interpolate=interpolate,
+                load_dotenv(dotenv_path, override=override, interpolate=interpolate,
                             **kwargs)
     return True
 
